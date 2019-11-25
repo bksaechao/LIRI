073fc621e72f409acfe00a971e9e7ebf8231e51c
CaoKao <bksaechao@gmail.com>
2019-11-25 14:40:42 -0800

    set up a switch statement to handle required commands

--- liri.js
+++ liri.js
@@ -1,7 +1,28 @@
 require('dotenv').config();
-
+// Loading required packages
+const fs = require("fs");
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 const axios = require("axios");
 const moment = require("moment");
 const spotify = new Spotify(keys.spotify);
+
+//Taking 2 arguments in the node application
+//First argument will be the user input()
+var userInput = process.argv[2]
+var userQuery = process.argv[3]
+
+switch (userInput) {
+    case "conert-this":
+        concert();
+        break;
+    case "spotify-this-song":
+        spotify();
+        break;
+    case "movie-this":
+        movie();
+        break;
+    case "do-what-it-says":
+        doIt();
+        break;
+}
