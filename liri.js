1f9b89ff27968dde593d4000b574acd12f7b1616
CaoKao <bksaechao@gmail.com>
2019-11-26 03:06:58 -0800

    successfully created a function to log and display required concert information in the node terminal and added in a log.txt to append logs

--- liri.js
+++ liri.js
@@ -1,28 +1,70 @@
 require('dotenv').config();
-// Loading required packages
+// Loading required packages.
 const fs = require("fs");
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 const moment = require("moment");
-const spotify = new Spotify(keys.spotify);
 
-//Taking 2 arguments in the node application
-//First argument will be the user input()
-var userInput = process.argv[2]
-var userQuery = process.argv[3]
+// Taking 2 arguments in the node application.
+// First argument will be the command(i.e "concert-this" "spotify-this-song" etc..).
+// Second argument will be the specified content being queried.
+var command = process.argv[2];
+var input = process.argv[3];
 
-switch (userInput) {
-    case "conert-this":
-        concert();
+// Switch statement to handle commands.
+
+switch (command) {
+    case "concert-this":
+        concertThis();
         break;
     case "spotify-this-song":
-        spotify();
+        spotifyThis();
         break;
     case "movie-this":
-        movie();
+        movieThis();
         break;
     case "do-what-it-says":
-        doIt();
+        doThis();
         break;
+}
+
+function concertThis() {
+    var artist = input;
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(response => {
+        console.log("Venu: " + response.data[0].venue.name + "\n");
+        console.log("Location: " + response.data[0].venue.city + "\n");
+        console.log("Date: " + moment(response.data[0].datetime).format("MM-DD-YYYY"));
+    }).catch(error => {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
+};
+
+function spotifyThis() {
+    console.log('hello');
+};
+
+function movieThis() {
+
+}
+
+function doThis() {
+
 }
